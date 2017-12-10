bc8d42b60a0eca42b2f9c2945beebabdbe2ca0a2
lorenzotrap <33368097+lorenzotrap@users.noreply.github.com>
2017-12-10 20:31:54 +0100

    Update index .php

--- index .php
+++ index .php
@@ -1,26 +1,20 @@
-<?php
-    if(!isset($_COOKIE["conta"]))
-        setcookie("conta", 1, time()+86400*7);
-    else
-    {
-        $_COOKIE["conta"]++;
-        setcookie("conta", $_COOKIE["conta"], time()+86400*7);
-    }
-?>
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <meta charset="iso-8859-1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Prova Cookie</title>
-</head>
-<body>
-<?php
-    if(isset($_COOKIE["conta"]))
-        echo "<h1>Bentornato, hai eseguito l'accesso ".$_COOKIE["conta"]." volte</h1>"; 
-    else
-        echo "<h1>Benvenuto, conterò i tuoi accessi per una settimana</h1>";    
-?>
-</body>
-</html>
+const request = require('request');
+
+request({
+  url: 'https://api.foursquare.com/v2/venues/explore',
+  method: 'GET',
+  qs: {
+    client_id: 'FJSNCX5OSZQ3H4S3BCIIBEAMXU0XFOPK22CNRXNKQXTXHNGT',
+    client_secret: 'BWGFBQSQYNRODHUZPUHPQE55KQ04XVX0T2LIOPMYGDQ2EAKM',
+    ll: '40.7243,-74.0018',
+    query: 'coffee',
+    v: '20170801',
+    limit: 1
+  }
+}, function(err, res, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(body);
+  }
+});
